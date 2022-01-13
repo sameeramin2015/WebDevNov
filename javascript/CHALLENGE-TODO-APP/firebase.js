5bba67d53543bd9c92068486b7d457589a4cd849
sameeramin2015 <sameeramin2015@gmail.com>
2022-01-13 20:06:03 +0100

    TO DO LIVE app

--- javascript/CHALLENGE-TODO-APP/firebase.js
+++ javascript/CHALLENGE-TODO-APP/firebase.js
@@ -1,4 +1,4 @@
-var firebaseConfig = {
+/*var firebaseConfig = {
     apiKey: "AIzaSyCXwhRFIHPA6QZYyS2hsynBBIeyWiL9X7E",
     authDomain: "to-do-live-d32f2.firebaseapp.com",
     projectId: "to-do-live-d32f2",
@@ -12,3 +12,21 @@ var firebaseConfig = {
     firebase.initializeApp(firebaseConfig);
     firebase.getAnalytics();
     var db = firebase.firestore();
+*/
+//import { initializeApp } from 'firebase/app';
+//import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCXwhRFIHPA6QZYyS2hsynBBIeyWiL9X7E",
+    authDomain: "to-do-live-d32f2.firebaseapp.com",
+    projectId: "to-do-live-d32f2",
+    storageBucket: "to-do-live-d32f2.appspot.com",
+    messagingSenderId: "164616315710",
+    appId: "1:164616315710:web:b756cb4c8cd29e1921026f",
+    measurementId: "G-1W66WVSZLZ"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  var db = firebase.firestore();
--- javascript/CHALLENGE-TODO-APP/script.js
+++ javascript/CHALLENGE-TODO-APP/script.js
@@ -1,10 +1,9 @@
 function addItem(event){
     event.preventDefault();
     let text = document.getElementById("todo-input");
-    db.collection("todo-items").add({
+    let newItem = db.collection("todo-items").add({
         text: text.value,
         status: "active"
-
     })
-    
+    text.value = "";
 }
