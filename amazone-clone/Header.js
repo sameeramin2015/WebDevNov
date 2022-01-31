1a38005fc2f836fb142a66b77cfbda1a7350ea26
sameeramin2015 <93704415+sameeramin2015@users.noreply.github.com>
2022-01-31 23:04:54 +0100

    Create Header.js

---
+++ amazone-clone/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+function Header() {
+  
+  return <nav className="header">
+     
+        <img
+         
+          src="./src/amazone_PNG11.png"
+          alt=""/>
+      
+
+    </nav>
+  
+}
+
+export default Header;
